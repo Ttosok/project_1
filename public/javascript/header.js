6574b3a02508b8a90e22b4bbdacf246fc3256bb4
Ttosok <ttosok97@gmail.com>
2023-06-06 15:29:43 +0700

    every basic web is done,there 2 version of unav-item in html using the div then ul

--- public/javascript/header.js
+++ public/javascript/header.js
@@ -1,29 +1,91 @@
 var Header_main = document.getElementById("header_content");
-var head_menu = document.querySelector(".head_containter-sort.menu-toggole");
+
 window.addEventListener('scroll', (e) => {
     // console.log(window.outerWidth + " outterwidth");
     // console.log(window.pageXOffset + " pageXOffset");
     // console.log(window.pageYOffset + " pageYOffset");
     if(this.scrollY > 25){
         // head_menu.style.transform = "translateY(-40px)";
-        head_menu.setAttribute('class', "head_containter-sort menu-toggole captured");
-        Header_main.style.position = "fixed";
-        Header_main.querySelector('.containter_tool-search').setAttribute('class', "containter_tool-search");
-        Header_main.querySelector('.head_right-middle').setAttribute('class', "head_right-middle hidden");
-        Header_main.querySelector('.head_left .btn_menu').setAttribute('class', "btn_menu");
+
+        // head_menu.setAttribute('class', "head_containter-sort menu-toggole captured");
+        // Header_main.style.position = "fixed";
+        // Header_main.querySelector('.containter_tool-search').setAttribute('class', "containter_tool-search");
+        // Header_main.querySelector('.head_right-middle').setAttribute('class', "head_right-middle hidden");
+        // Header_main.querySelector('.head_left .btn_menu').setAttribute('class', "btn_menu");
+
         // Header_main.querySelector('.containter_tool').style.justifyContent = "flex-end";
     }else{
         // head_menu.style.transform = "translateY(0px)";
-        head_menu.setAttribute('class', "head_containter-sort menu-toggole");
-        Header_main.style.position = "absolute";
-        Header_main.querySelector('.containter_tool-search').setAttribute('class', "containter_tool-search hidden");
-        Header_main.querySelector('.head_right-middle').setAttribute('class', "head_right-middle");
-        Header_main.querySelector('.head_left .btn_menu').setAttribute('class', "btn_menu hidden");
+
+        // head_menu.setAttribute('class', "head_containter-sort menu-toggole");
+        // Header_main.style.position = "absolute";
+        // Header_main.querySelector('.containter_tool-search').setAttribute('class', "containter_tool-search hidden");
+        // Header_main.querySelector('.head_right-middle').setAttribute('class', "head_right-middle");
+        // Header_main.querySelector('.head_left .btn_menu').setAttribute('class', "btn_menu hidden");
+
         // Header_main.querySelector('.containter_tool').style.justifyContent = "flex-start";
     }
 });
+var head_menu = document.querySelector(".menu-toggole");
+function Menu_clicked_Hor(){
+    console.log("checking for horizon");
+}
+function Menu_clicked_Ver(){
+    console.log("checking for vertical");
+}
+var btn_menu = document.querySelector(".btn_menu");
+btn_menu.addEventListener('click', (event) =>{ /*  && head_menu.getAttribute("class").includes("focus") */ 
+    head_menu.getAttribute("class").includes("side-menu")?
+    Menu_clicked_Hor() : Menu_clicked_Ver();
+
+    // if(head_menu.getAttribute("class").includes("focus"))
+    // {
+    //     for (const kid of  Header_main.querySelector('.head_left .btn_menu-icon').children) {
+    //         console.log(`${kid.className == "hidden" ? kid.className + "before  here" : kid.className + " before here"}`);
+    //         kid.className == "hidden" ? kid.classList.remove("hidden") : kid.classList.add("hidden");
+    //     }
+    //     head_menu.setAttribute('class', "head_containter-sort menu-toggole captured");
+    // }else{
+    //     head_menu.setAttribute('class', "head_containter-sort menu-toggole captured focus");
+    //     for (const kid of  Header_main.querySelector('.head_left .btn_menu-icon').children) {
+    //         console.log(`${kid.className == "hidden" ? kid.className + " fater here" : kid.className + " after  here"}`);
+    //         kid.className == "hidden" ? kid.classList.remove("hidden") : kid.classList.add("hidden");
+    //     }
+    // }
+    head_menu.getAttribute("class").includes("focus") ?
+    head_menu.setAttribute('class', "head_containter-sort menu-toggole captured") : head_menu.setAttribute('class', "head_containter-sort menu-toggole captured focus");
+    for (const kid of  Header_main.querySelector('.head_left .btn_menu-icon').children) {
+        console.log(`${kid.className == "hidden" ? kid.className + " no hidden here" : kid.className + " after hidden here"}`);
+        kid.className == "hidden" ? kid.classList.remove("hidden") : kid.classList.add("hidden");
+    }
+});
+var Array_unavLi = document.querySelectorAll(".unav-li");
+var Array_unavItem = document.querySelectorAll(".unav-item")
+var All_Body_contain = document.getElementById("body_content")
 window.addEventListener("resize", (e) =>{
     console.log(this.innerWidth + " innerwidth");
+    if(this.innerWidth < 1600){
+        All_Body_contain.style.top = "110px";
+        btn_menu.setAttribute('class', "btn_menu hidden");
+        head_menu.setAttribute('class', "head_containter-sort menu-toggole");
+        for(const unavLi of Array_unavLi){
+            unavLi.setAttribute('class', "unav-li");
+        }
+        for(const unavitem of Array_unavItem){
+            unavitem.setAttribute('class', "unav-item hidden");
+        }
+    }
+    if(this.innerWidth < 992){
+        All_Body_contain.style.top = "40px";
+        btn_menu.setAttribute('class', "btn_menu");
+        head_menu.setAttribute('class', "head_containter-sort menu-toggole hidden");
+        for(const unavLi of Array_unavLi){
+            unavLi.setAttribute('class', "unav-li hidden");
+        }
+        for(const unavitem of Array_unavItem){
+            unavitem.setAttribute('class', "unav-item side-menu");
+        }
+    }
 });
 // window.addEventListener('click', (event) =>{
 //     console.log(event.target);
@@ -104,33 +166,11 @@ head_support.addEventListener('click', (event) =>{
     }
 });
 var head_searchScroll = document.getElementById("searchlink");
-var btn_menu = document.querySelector(".btn_menu");
 head_searchScroll.addEventListener('click', (event) =>{
     Header_main.querySelector('.head_right-middle').setAttribute('class', "head_right-middle");
     Header_main.querySelector('.containter_tool-search').setAttribute('class', "containter_tool-search hidden");
 });
-btn_menu.addEventListener('click', (event) =>{
-    // if(head_menu.getAttribute("class").includes("focus"))
-    // {
-    //     for (const kid of  Header_main.querySelector('.head_left .btn_menu-icon').children) {
-    //         console.log(`${kid.className == "hidden" ? kid.className + "before  here" : kid.className + " before here"}`);
-    //         kid.className == "hidden" ? kid.classList.remove("hidden") : kid.classList.add("hidden");
-    //     }
-    //     head_menu.setAttribute('class', "head_containter-sort menu-toggole captured");
-    // }else{
-    //     head_menu.setAttribute('class', "head_containter-sort menu-toggole captured focus");
-    //     for (const kid of  Header_main.querySelector('.head_left .btn_menu-icon').children) {
-    //         console.log(`${kid.className == "hidden" ? kid.className + " fater here" : kid.className + " after  here"}`);
-    //         kid.className == "hidden" ? kid.classList.remove("hidden") : kid.classList.add("hidden");
-    //     }
-    // }
-    head_menu.getAttribute("class").includes("focus") ?
-    head_menu.setAttribute('class', "head_containter-sort menu-toggole captured") : head_menu.setAttribute('class', "head_containter-sort menu-toggole captured focus");
-    for (const kid of  Header_main.querySelector('.head_left .btn_menu-icon').children) {
-        console.log(`${kid.className == "hidden" ? kid.className + " fater here" : kid.className + " after  here"}`);
-        kid.className == "hidden" ? kid.classList.remove("hidden") : kid.classList.add("hidden");
-    }
-});
+
 // const Father =  Header_main.querySelector('.head_left .btn_menu-icon');
 // for (const child of Father.children) {
 //     console.log(`${child.className == "hidden" ? "there is hidden here" : " no hidden here"}`);
